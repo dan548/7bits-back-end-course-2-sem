1ca1a941e7aa142cee9799688304a0ab7fe49280
Даниил Поляков <danfromomsk@gmail.com>
2019-08-05 16:57:36 +0600

    fixed again and again

--- HW-Spring/src/main/java/it/sevenbits/spring_homework/config/WebSecurityConfig.java
+++ HW-Spring/src/main/java/it/sevenbits/spring_homework/config/WebSecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.web.access.intercept.FilterSecurityIntercept
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 /**
  * Security configuration
@@ -52,14 +57,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().disable();
         http.requestCache().disable();
         http.anonymous();
-        http.cors();
-        
+
         RequestMatcher signUpPageMatcher = new AntPathRequestMatcher("/signup");
         RequestMatcher notSignUpPageMatcher = new NegatedRequestMatcher(signUpPageMatcher);
         JwtAuthFilter authFilter = new HeaderJwtAuthFilter(notSignUpPageMatcher);
         http.addFilterBefore(authFilter, FilterSecurityInterceptor.class);
 
         http
+                .cors()
+                .and()
                 .authorizeRequests().antMatchers("/users/**").hasAuthority("ADMIN")
                 .and()
                 .authorizeRequests().antMatchers("/whoami").hasAuthority("USER")
@@ -74,10 +80,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(new JwtAuthenticationProvider(jwtTokenService));
     }
 
-
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/signin");
         web.ignoring().antMatchers("/signup");
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Origin", "Accept", "Key",
+                "DNT", "Keep-Alive", "User-Agent", "X-Requested-With",
+                "If-Modified-Since", "Cache-Control", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Location"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
